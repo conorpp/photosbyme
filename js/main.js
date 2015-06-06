48e239665d2664c9f1f757eadedc85166f79a284
Conor <conorpp@vt.edu>
2015-06-06 12:22:00 -0400

    got rid of scroll

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ test = 0;
 
 $(document).ready(function(){
 
-    //$('#myname').velocity('slideUp', {delay:200, duration:500});
 
-    var initFontSize = $('#scroll').css('font-size').replace('px','');
+    //var initFontSize = $('#scroll').css('font-size').replace('px','');
 
     var scrollEle= $('#scroll');
     var imgListEle= $('#image-list');
     var em = 0;
-    var winheight = $(window).height();
     var licon = $('#imgjail').find('.spinner');
     $('#imgjail').find('img').each(function(i){
         var img = $(this);
@@ -19,52 +17,59 @@ $(document).ready(function(){
         $('#image-list').append(img);
         $('#image-list').append(nlicon);
         img.on('load',function(){
-            setTimeout(function(){
-                    $('#'+id).hide();
-                    //img.show(1000);
-                    console.log('show');
-            },2000);
+            $('#'+id).hide();
         });
     });
-
-
-    $(document).on('scroll',function(e){
+    
+    var checkimgs = function(e){
         var docscrolltop = $(document).scrollTop();
         var viewheight = isNaN(window.innerHeight) ? window.clientHeight : window.innerHeight;
+        var winheight = $(window).height();
         $('img.invis').each(function(){
             var val = $(this).offset().top - docscrolltop ;
             if(val < 300 || docscrolltop + viewheight/2 >= winheight)
-            {
-                $(this).css('visibility','visible');
-                $(this).velocity({opacity:1},{duration:1000});
-            }
+        {
+            console.log(val,winheight);
+            $(this).css('visibility','visible');
+            $(this).velocity({opacity:1},{duration:1000});
+        }
         });
         test = e;
         var inc = 9;
         var imgListEle= $('#image-list');
-        var t = imgListEle.offset().top-100;
-        var fs = scrollEle.css('font-size').replace('px','');
-        fs = parseInt(fs);
-        fs -= inc;
-        fs = (t - docscrolltop)/t * initFontSize;
-        if (fs <= 0)
+        if (docscrolltop > 80)
         {
-            fs = 0; 
-            if (em != -1)
-            {
-                $('#myname').css('margin-top','-1em');
-                em = -1;
-            }
+            $('#myname').css('margin-top','-1em');
         }
         else
         {
-            if (em != 0)
-            {
-                $('#myname').css('margin-top','');
-                em = 0;
-            }
+            $('#myname').css('margin-top','');
         }
-        scrollEle.css('font-size',fs+'px');
-    });
+        //var t = imgListEle.offset().top-100;
+        //var fs = scrollEle.css('font-size').replace('px','');
+        //fs = parseInt(fs);
+        //fs -= inc;
+        //fs = (t - docscrolltop)/t * initFontSize;
+        /*
+           if (fs <= 0)
+           {
+           fs = 0; 
+           if (em != -1)
+           {
+           em = -1;
+           }
+           }
+           else
+           {
+           if (em != 0)
+           {
+           em = 0;
+           }
+           }
+           */
+    } 
+
+    $(document).on('scroll',checkimgs);
+    checkimgs();
 
 });
